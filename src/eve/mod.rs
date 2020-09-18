73390bc755fd28056e2d3943b2f15fee526e744e
dbcfd <bdbrowning2@gmail.com>
2020-09-18 10:19:35 -0600

    Expose state

--- src/eve/mod.rs
+++ src/eve/mod.rs
@@ -7,6 +7,6 @@ pub use message::{
     DnsEventType as EveDnsEventType, DnsInfo as EveDnsInfo, DnsQuery as EveDnsQuery,
     EventFields as EveEventFields, EventType as EveEventType, Flow as EveFlow,
     FlowInfo as EveFlowInfo, Http as EveHttp, Ja3 as EveJa3, Message as EveMessage,
-    Smtp as EveSmtp, Stats as EveStats, Tls as EveTls,
+    Smtp as EveSmtp, State as EveState, Stats as EveStats, Tls as EveTls,
 };
 pub use reader::EveReader;
