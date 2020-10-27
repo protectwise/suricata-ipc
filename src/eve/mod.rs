95840ab9a0e8896329a53acb1b8b5a4e27d713ff
ben kornmeier <ben@protectwise.com>
2020-10-27 12:01:06 -0600

    expose AlertInfo

--- src/eve/mod.rs
+++ src/eve/mod.rs
@@ -3,7 +3,7 @@ mod message;
 mod reader;
 
 pub use message::{
-    parse_date_time, Alert as EveAlert, Dns as EveDns, DnsAnswer as EveDnsAnswer,
+    parse_date_time, Alert as EveAlert, AlertInfo as EveAlertInfo, Dns as EveDns, DnsAnswer as EveDnsAnswer,
     DnsEventType as EveDnsEventType, DnsInfo as EveDnsInfo, DnsQuery as EveDnsQuery,
     EventFields as EveEventFields, EventType as EveEventType, Flow as EveFlow,
     FlowInfo as EveFlowInfo, Http as EveHttp, Ja3 as EveJa3, Message as EveMessage,
