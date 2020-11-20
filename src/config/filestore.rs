79f45deac04e7d68b626028612456ff367fc12f9
dbcfd <bdbrowning2@gmail.com>
2020-11-20 10:49:39 -0700

    File Extraction Fixes
    Add additional states for file events. If using default filestore path, create
    directory if it does not exist.

--- src/config/filestore.rs
+++ src/config/filestore.rs
@@ -1,3 +1,5 @@
+use crate::Error;
+
 use askama::Template;
 use std::path::PathBuf;
 
@@ -15,18 +17,24 @@ impl Filestore {
         }
     }
 
-    pub fn render(&self, materialize_path: &PathBuf) -> String {
+    pub fn render(&self, materialize_path: &PathBuf) -> Result<String, Error> {
         let path = if let Some(ref p) = self.path {
             p.clone()
         } else {
-            materialize_path.join("filestore")
+            let p = materialize_path.join("filestore");
+            log::info!("Setting up default filestore path {:?}", p);
+            if !p.exists() {
+                log::info!("Default filestore path {:?} did not exist", p);
+                std::fs::create_dir_all(&p)?;
+            }
+            p
         };
         let enabled = if self.enabled { "true" } else { "false" };
         let r = RenderedFilestore {
             enabled: enabled,
             path: path.to_string_lossy(),
         };
-        r.render().unwrap()
+        r.render().map_err(Error::Askama)
     }
 }
 
--- src/eve/message/file.rs
+++ src/eve/message/file.rs
@@ -1,13 +1,35 @@
 use serde::{Deserialize, Serialize};
 
+/// https://github.com/OISF/suricata/blob/master/src/util-file.h#L52
+/// https://github.com/OISF/suricata/blob/master/src/output-json.c#L156
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum FileState {
+    #[serde(rename = "NONE")]
+    None,
     #[serde(rename = "OPEN")]
     Open,
     #[serde(rename = "CLOSED")]
     Closed,
     #[serde(rename = "UNKNOWN")]
     Unknown,
+    #[serde(rename = "TRUNCATED")]
+    Truncated,
+    #[serde(rename = "ERROR")]
+    Error,
+}
+
+impl std::fmt::Display for FileState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::None => "none",
+            Self::Open => "open",
+            Self::Closed => "closed",
+            Self::Unknown => "unknown",
+            Self::Truncated => "truncated",
+            Self::Error => "error",
+        };
+        write!(f, "{}", s)
+    }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
